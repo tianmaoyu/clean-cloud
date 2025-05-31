fff67c46bb22efb022ccadbdcb9490fd6c54de62
eric <tianmaoyu@foxmail.com>
2025-05-31 22:46:01 +0800

    init

---
+++ clean-common/clean-starter-mybatis/src/test/java/org/clean/MainTest.java
@@ -0,0 +1,15 @@
+package org.clean;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    void main() {
+    }
+
+    @Test
+    void testMain() {
+    }
+}
