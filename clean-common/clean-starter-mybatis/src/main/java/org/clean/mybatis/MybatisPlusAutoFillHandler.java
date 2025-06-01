8dcdba5f6f656dd283ec3f54d06ffb2a3d913224
eric <tianmaoyu@foxmail.com>
2025-06-01 10:32:27 +0800

    mybtis

---
+++ clean-common/clean-starter-mybatis/src/main/java/org/clean/mybatis/MybatisPlusAutoFillHandler.java
@@ -0,0 +1,33 @@
+package org.clean.mybatis;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class MybatisPlusAutoFillHandler implements MetaObjectHandler {
+
+    // 插入时触发填充
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        if (metaObject.getValue("createId") == null)
+            this.strictInsertFill(metaObject, "createId", Long.class, 10L);
+        if (metaObject.getValue("createTime") == null)
+            this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+        if (metaObject.getValue("updateId") == null)
+            this.strictInsertFill(metaObject, "updateId", Long.class, 10L);
+        if (metaObject.getValue("updateTime") == null)
+            this.strictInsertFill(metaObject, "updateTime", Date.class, new Date());
+    }
+
+    // 更新时触发填充
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        if (metaObject.getValue("updateId") == null)
+            this.strictUpdateFill(metaObject, "updateId", Long.class, 10L);
+        if (metaObject.getValue("updateTime") == null)
+            this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date());
+    }
+}
