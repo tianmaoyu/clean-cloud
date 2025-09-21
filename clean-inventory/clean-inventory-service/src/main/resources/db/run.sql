0dd9c8542ddbe990b0a319b7c4e72d4aadb8165c
eric <tianmaoyu@foxmail.com>
2025-09-21 23:47:55 +0800

    HASH 分区key 的修改

--- clean-inventory/clean-inventory-service/src/main/resources/db/run.sql
+++ clean-inventory/clean-inventory-service/src/main/resources/db/run.sql
@@ -47,6 +47,23 @@ CREATE TABLE product_part_5 PARTITION OF product_part FOR VALUES WITH (modulus 8
 CREATE TABLE product_part_6 PARTITION OF product_part FOR VALUES WITH (modulus 8, remainder 6);
 CREATE TABLE product_part_7 PARTITION OF product_part FOR VALUES WITH (modulus 8, remainder 7);
 
-insert into  product_part select * from product
+-- insert into  product_part select * from product
+
+explain select * from product_part  where category_id=1 limit 100
+
+-- 分区key修改，分区可以更新
+
+-- 6100000
+select count(*)  from product_part;
+-- 816855
+select count(*)  from product_part_4;
+-- 816850 执行 分区key 的修改后，会重新进入各自的分区
 
 
+-- product_part_4 [category_id=209]
+-- product_part_5 [category_id=309]
+-- product_part_0 [category_id=409]
+-- 修改分区key
+select tableoid::regclass, * from product_part where "name"  like 'Product 5120%' and category_id =209
+
+update product_part set category_id=309 where "name"  like 'Product 5120%' and category_id =209;
