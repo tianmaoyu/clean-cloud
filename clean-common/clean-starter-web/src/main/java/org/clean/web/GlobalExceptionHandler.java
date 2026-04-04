593291d412952ab93e8b42ce83f1d6ef023669d3
eric <tianmaoyu@foxmail.com>
2026-04-04 10:57:44 +0800

    更多的指标

--- clean-common/clean-starter-web/src/main/java/org/clean/web/GlobalExceptionHandler.java
+++ clean-common/clean-starter-web/src/main/java/org/clean/web/GlobalExceptionHandler.java
@@ -5,9 +5,12 @@ import feign.codec.DecodeException;
 import lombok.extern.slf4j.Slf4j;
 import org.clean.CleanException;
 import org.clean.Result;
+import org.springframework.dao.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLTimeoutException;
 import java.util.Arrays;
 
 @Slf4j
@@ -27,7 +30,33 @@ public class GlobalExceptionHandler {
         log.error("业务异常: {} -> {}", Arrays.stream(e.getStackTrace()).findFirst().orElse(null), e.getMessage());
         return Result.fail(e.getCode(), e.getMessage());
     }
+    @ExceptionHandler({DeadlockLoserDataAccessException.class})
+    public ResponseEntity<String> handleDeadlock(DeadlockLoserDataAccessException ex) {
+        log.error("数据库死锁错误: {}", ex.getMessage(), ex);
+        return ResponseEntity.status(500).body("数据库死锁，请稍后重试");
+    }
+    @ExceptionHandler({
+            QueryTimeoutException.class,
+            SQLTimeoutException.class,
+            DataAccessResourceFailureException.class
+    })
+    public ResponseEntity<String> handleDbTimeout(Exception ex) {
+        log.error("数据库访问超时: {}", ex.getMessage(), ex);
+        return ResponseEntity.status(504).body("数据库访问超时，请稍后重试");
+    }
 
+
+    @ExceptionHandler({DataIntegrityViolationException.class})
+    public ResponseEntity<String> handleDataIntegrity(DataIntegrityViolationException ex) {
+        log.error("数据完整性错误: {}", ex.getMessage(), ex);
+        return ResponseEntity.badRequest().body("数据库字段或约束问题");
+    }
+
+    @ExceptionHandler({DataAccessException.class})
+    public ResponseEntity<String> handleDataAccess(DataAccessException ex) {
+        log.error("数据库访问错误: {}", ex.getMessage(), ex);
+        return ResponseEntity.status(500).body("数据库访问异常");
+    }
 
     /**
      * 拦截 DecodeException 异常，decoder 中抛出的自定义全局异常会进入此处
